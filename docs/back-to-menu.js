2c2235a43f5adb921978c0a5b4070d3a5ec945de
Krystal Ng <krystal.ng@ecs.vuw.ac.nz>
2025-06-30 16:56:27 +1200

    fixed the filepath for github pages

--- docs/back-to-menu.js
+++ docs/back-to-menu.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const backBtn = document.getElementById('backBtn');
   if (backBtn) {
     backBtn.addEventListener('click', () => {
-      window.location.href = "menu.html";
+      window.location.href = "./menu.html";
     });
   }
 });
