8102acf31cbaf3205e2b037f08ed053229939b1d
Krystal Ng <krystal.ng@ecs.vuw.ac.nz>
2025-06-30 17:00:54 +1200

    fixed render.js

--- docs/render.js
+++ docs/render.js
@@ -2,14 +2,22 @@ window.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.getElementById('menuBtn');
   if (menuBtn) {
     menuBtn.addEventListener('click', () => {
-      window.electronAPI.goToMenu();
+      if (window.electronAPI) {
+        window.electronAPI.goToMenu();  // Electron mode
+      } else {
+        window.location.href = './menu.html';  // Web mode
+      }
     });
   }
 
   const homeBtn = document.getElementById('homeBtn');
   if (homeBtn) {
     homeBtn.addEventListener('click', () => {
-      window.electronAPI.goToHome();
+      if (window.electronAPI) {
+        window.electronAPI.goToHome();
+      } else {
+        window.location.href = './index.html';
+      }
     });
   }
 });
